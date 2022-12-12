b194a5808d8460df5f924b7e4dfbd328f4a7a876
Ruben Engels <16747379+RMEngels@users.noreply.github.com>
2022-12-12 09:33:44 +0100

    Write heading line

--- dist/index.js
+++ dist/index.js
@@ -90,7 +90,7 @@ var CommentFormatter = /** @class */ (function () {
     };
     CommentFormatter.prototype.heading = function () {
         var heading = '```diff\n';
-        // heading += (this.coverageDiff.linesDiff > 0) ? '+' : '-' + 'lines: from ' + this.headCoverage.linesCoverage + '% to ' + this.coverage.linesCoverage +'% (' + (this.coverageDiff.linesDiff > 0) ? '+' : '-' + this.coverageDiff.linesDiff + '%)\n';
+        heading += ((this.coverageDiff.linesDiff > 0) ? '+' : '-') + ' lines: from ' + this.headCoverage.linesCoverage.toFixed(2) + '% to ' + this.coverage.linesCoverage.toFixed(2) + '% (' + ((this.coverageDiff.linesDiff > 0) ? '+' : '-') + this.coverageDiff.linesDiff.toFixed(2) + '%)\n';
         heading += '- deze moet ik nog fixen huil huil\n';
         heading += '```\n\n';
         return heading;
--- src/Actions/CommentFormatter.ts
+++ src/Actions/CommentFormatter.ts
@@ -27,7 +27,7 @@ export class CommentFormatter {
 	private heading(): string {
 		let heading = '```diff\n';
 
-		// heading += (this.coverageDiff.linesDiff > 0) ? '+' : '-' + 'lines: from ' + this.headCoverage.linesCoverage + '% to ' + this.coverage.linesCoverage +'% (' + (this.coverageDiff.linesDiff > 0) ? '+' : '-' + this.coverageDiff.linesDiff + '%)\n';
+		heading += ((this.coverageDiff.linesDiff > 0) ? '+' : '-') + ' lines: from ' + this.headCoverage.linesCoverage.toFixed(2) + '% to ' + this.coverage.linesCoverage.toFixed(2) +'% (' + ((this.coverageDiff.linesDiff > 0) ? '+' : '-') + this.coverageDiff.linesDiff.toFixed(2) + '%)\n';
 		heading += '- deze moet ik nog fixen huil huil\n';
 		heading += '```\n\n';
 
